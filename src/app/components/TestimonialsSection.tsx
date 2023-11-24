18e9b7a8c3762ffeaf174361e282941a83a72862
Sohyb01 <sohyb0155@gmail.com>
2023-11-24 22:21:56 +0200

    fix

--- src/app/components/TestimonialsSection.tsx
+++ src/app/components/TestimonialsSection.tsx
@@ -6,7 +6,7 @@ const TestimonialsSection = () => {
     <section className="py-10 md:py-16">
       <div className="container max-w-screen-xl mx-auto px-4">
         <h1 className="font-medium text-gray-700 text-3xl mb-5 title-text">
-          Certificates
+          Awards
         </h1>
 
         <p className="font-normal text-gray-500 text-base mb-8">
